e2c101be776a297798cd3373fe40e3ed6d2071f5
jefriedm <jsf6410@g.rit.edu>
2016-08-10 12:14:59 -0400

    Debugging OpenShift build error.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jefriedm/git/metrictool/venv"
+set -gx VIRTUAL_ENV "/home/jefriedm/git/metrics/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
